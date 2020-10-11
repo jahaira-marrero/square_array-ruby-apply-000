270fac676db011df352eac9cce2bf960dad4bfc5
Jahaira Marrero <jahaira.marrero@gmail.com>
2020-10-11 04:15:44 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,9 @@
 def square_array(numbers)
-  square_numbers = []
+  
+  squared_numbers = []
   numbers.each do |num|
-    squared << (num ** 2)
-end
-return squared_numbers
+    squared_numbers << (num ** 2)
+ end
+ return squared_numbers
+
 end
